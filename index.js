0d735957c60f397a757e3d272e6516f1517fb688
Nour Ayyoub <nour.ayyoub360@gmail.com>
2023-10-22 19:50:03 +0300

    creat-footer

--- index.js
+++ index.js
@@ -23,5 +23,21 @@ document.styleSheets[0].insertRule(".navbar a:hover { background-color: #ddd;col
 
 logo.style.cssText = "width:60px;height:60px; border-radius: 20px;"
 
-//console.log(navbar)
 document.body.appendChild(navbar)
+
+//////////////////////////////////////////////////////////////////
+
+let footer = document.createElement("footer")
+let p = document.createElement("p")
+let text_p = document.createTextNode("CopyRights ")
+let span_p = document.createElement("span")
+
+let  currentYear = new Date().getFullYear()
+
+p.appendChild(text_p) 
+p.innerHTML += currentYear
+footer.appendChild(p)
+
+document.styleSheets[0].insertRule("footer { position: fixed; left: 0; bottom: 0; width: 100%; background-color: #333; color: white; text-align: center;}")
+
+document.body.appendChild(footer)

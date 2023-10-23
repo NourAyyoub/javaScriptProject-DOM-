dfc9c0e7d3511f8a09a4864f6910049dec5a2573
Nour Ayyoub <nour.ayyoub360@gmail.com>
2023-10-23 11:35:11 +0300

    create-container

--- index.js
+++ index.js
@@ -41,3 +41,33 @@ footer.appendChild(p)
 document.styleSheets[0].insertRule("footer { position: fixed; left: 0; bottom: 0; width: 100%; background-color: #333; color: white; text-align: center;}")
 
 document.body.appendChild(footer)
+/////////////////////////////////////////////////////////////////
+let container = document.createElement("div")
+
+container.className = "card-container"
+
+let number_of_cards = 35
+for(let i=1 ; i<=number_of_cards ; i++)
+{
+    let card = document.createElement("div")
+    let h3 = document.createElement("h3")
+    let h3_p = document.createElement("p")
+    
+    let h3_text = document.createTextNode(i)
+    let h3_p_text = document.createTextNode("Product")
+
+    h3.appendChild(h3_text)
+    h3_p.appendChild(h3_p_text)
+
+    card.appendChild(h3)
+    card.appendChild(h3_p)
+
+    card.className = "card"
+
+    container.appendChild(card)
+}
+
+document.styleSheets[0].insertRule(".card-container { text-align: center; display: inline-block;}")
+document.styleSheets[0].insertRule(".card { box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); width: 200px; margin: 15px; text-align: center; font-family: arial; display: inline-block; }")
+
+document.body.appendChild(container)
